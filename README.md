41081baee14f66b5bc27595b2fc16f4ed3b9ea60
Dimas Sudjito <almuhtad@ualberta.ca>
2022-01-20 23:24:49 -0700

    integrate canvas sketching to react

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 - An experimental graphic design app
 - built with: Typescript, React
+
+# Reference
+
+- [Canvas HTML by Dev Ed](https://www.youtube.com/watch?v=3GqUM4mEYKA)
+- [Canvas + React by Maksim Ivanov](https://github.com/satansdeer/drawing-react-canvas/tree/master/src)
--- frontend/src/pages/Sketch.tsx
+++ frontend/src/pages/Sketch.tsx
@@ -1,5 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 export const Sketch: React.FC = () => {
-  return <div>Sketch</div>
+  const [isDrawing, setIsDrawing] = useState(false)
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const contextRef = useRef<CanvasRenderingContext2D | null>(null)
+
+  const prepareCanvas = () => {
+    const canvas = canvasRef.current
+    if (canvas) {
+      canvas.width = window.innerWidth * 2
+      canvas.height = window.innerHeight * 2
+      canvas.style.width = `${window.innerWidth}px`
+      canvas.style.height = `${window.innerHeight}px`
+
+      const context = canvas.getContext('2d')
+      if (context) {
+        context.scale(2, 2)
+        context.lineCap = 'round'
+        context.strokeStyle = 'black'
+        context.lineWidth = 5
+        contextRef.current = context
+      }
+    }
+  }
+
+  const startDrawing = ({ nativeEvent }: { nativeEvent: any }) => {
+    const { offsetX, offsetY } = nativeEvent
+    if (contextRef.current) {
+      contextRef.current.beginPath()
+      contextRef.current.moveTo(offsetX, offsetY)
+    }
+    setIsDrawing(true)
+  }
+
+  const finishDrawing = () => {
+    if (contextRef.current) {
+      contextRef.current.closePath()
+    }
+    setIsDrawing(false)
+  }
+
+  const draw = ({ nativeEvent }: { nativeEvent: any }) => {
+    if (!isDrawing) {
+      return
+    }
+    const { offsetX, offsetY } = nativeEvent
+    if (contextRef.current) {
+      contextRef.current.lineTo(offsetX, offsetY)
+      contextRef.current.stroke()
+    }
+  }
+
+  // const clearCanvas = () => {
+  //   const canvas = canvasRef.current
+  //   if (canvas) {
+  //     const context = canvas.getContext('2d')
+  //     if (context) {
+  //       context.fillStyle = 'white'
+  //       context.fillRect(0, 0, canvas.width, canvas.height)
+  //     }
+  //   }
+  // }
+
+  useEffect(() => {
+    prepareCanvas()
+  }, [])
+
+  return (
+    <canvas
+      onMouseDown={startDrawing}
+      onMouseUp={finishDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
+    ></canvas>
+  )
 }
