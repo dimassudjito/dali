44ebe16df7c619d536567f350e5280126967720a
Dimas Sudjito <almuhtad@ualberta.ca>
2022-04-08 18:35:45 -0600

    set textarea and bind to a state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import { Dashboard, Sketch, Notepad } from './pages'
 import { Header } from './components/dashboard'
--- frontend/src/pages/Notepad.tsx
+++ frontend/src/pages/Notepad.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, Grid, TextareaAutosize } from '@mui/material'
 
 export const Notepad: React.FC = () => {
+  const [text, setText] = useState('')
+
   return (
-    <div>
-      <h1>Notepad</h1>
-    </div>
+    <Box>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <TextareaAutosize
+            aria-label="Textarea to type in your content"
+            minRows={5}
+            maxRows={30}
+            placeholder="Type in your content here"
+            style={{ width: '100%' }}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          photo area
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
